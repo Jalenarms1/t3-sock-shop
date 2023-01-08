1206c1a5b1322c1d702671075c9b53e4e0e217cc
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2023-01-08 13:31:55 -0700

    adding more style to sock card

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,6 +4,7 @@ import {MdOutlineInventory} from "react-icons/md/index"
 import {TbReportMoney} from "react-icons/tb/index";
 import {BsBorderStyle} from "react-icons/bs/index"
 import {MdOutlineAccountCircle} from "react-icons/md/index"
+import {AiOutlineLogout} from "react-icons/ai/index"
 
 import { useState } from "react";
 
@@ -15,7 +16,7 @@ export default function NavBar() {
     <nav className="h-fit bg-slate-900 shadow-sm shadow-slate-700  px-5 text-2xl ">
           <div className="flex justify-between items-center py-4">
             <div className="text-2xl text-slate-300 font-semibold bg-[#411a1a] shadow-inner shadow-rose-900 px-2 py-1 rounded-md">
-              Socks &aposN Bottoms
+              Socks N Bottoms
             </div>
             <RxHamburgerMenu onClick={() => setShowOpt(!showOpt)} className="text-slate-100 active:text-slate-400" />
 
@@ -37,6 +38,10 @@ export default function NavBar() {
             <div className="border-b border-slate-800 pb-1 flex justify-between">
               <p className="text-sm text-slate-200 hover:text-slate-400 active:text-slate-500">Account</p>
               <MdOutlineAccountCircle className="text-slate-200 active:text-slate-400" />
+            </div>
+            <div className="border-b border-slate-800 pb-1 flex justify-between">
+              <p className="text-sm text-slate-200 hover:text-slate-400 active:text-slate-500">Logout</p>
+              <AiOutlineLogout className="text-slate-200 active:text-slate-400 text-md" />
             </div>
             
           </div>}
